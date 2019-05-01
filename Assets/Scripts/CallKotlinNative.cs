032e4db13c55d74f405502b3db45077e044d5ed1
MizoTake <mizotake123@gmail.com>
2019-05-01 23:14:05 +0900

    first

---
+++ Assets/Scripts/CallKotlinNative.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CallKotlinNative : MonoBehaviour
+{
+    private Text viewableText;
+
+    void Start()
+    {
+        var pluginMessage = "";
+#if UNITY_ANDROID
+        using (var plugin = new AndroidJavaObject("com.sample.mizotake.kotlinnativeforunity.common"))
+        {
+            pluginMessage = plugin.Call<string>("createApplicationScreenMessage");
+            Debug.Log(pluginMessage);
+        }
+#endif
+        viewableText = GetComponent<Text>();
+        viewableText.text = pluginMessage;
+    }
+}
